ecfbb6dbd9c4c0a248deef4f936e5dcd12ccd555
chishin <pro@xxgzs.org>
2019-12-18 20:12:33 +0800

    Init Project

---
+++ queue.c
@@ -0,0 +1,95 @@
+#include "queue.h"
+
+//==============================================================================
+//  函数声明
+//==============================================================================
+void Queue_Init(QUEUE_HandleTypeDef * hqueue, unsigned char * buffer, unsigned int len);
+void Queue_Clear(QUEUE_HandleTypeDef * hqueue);
+unsigned int Queue_Count(QUEUE_HandleTypeDef * hqueue);
+QUEUE_StatusTypeDef Queue_Push(QUEUE_HandleTypeDef * hqueue, unsigned char data);
+unsigned int Queue_Push_Array(QUEUE_HandleTypeDef * hqueue, unsigned char * pdatas, unsigned int len);
+
+//==============================================================================
+//  函数名称：
+//  函数功能：
+//  函数参数：
+//  函数返回：
+//==============================================================================
+void Queue_Init(QUEUE_HandleTypeDef * hqueue, unsigned char * buffer, unsigned int len)
+{
+    hqueue->buffer = buffer;
+    hqueue->buffer_length = len;
+    Queue_Clear(hqueue);
+}
+
+//==============================================================================
+//  函数名称：
+//  函数功能：
+//  函数参数：
+//  函数返回：
+//==============================================================================
+void Queue_Clear(QUEUE_HandleTypeDef * hqueue)
+{
+    hqueue->head = 0;
+    hqueue->tail = 0;
+}
+
+//==============================================================================
+//  函数名称：Queue_Count
+//  函数功能：获取队列内数据个数
+//  函数参数：
+//  函数返回：
+//==============================================================================
+unsigned int Queue_Count(QUEUE_HandleTypeDef * hqueue)
+{
+    if(hqueue->head <= hqueue->tail)
+    {
+        return (unsigned int)(hqueue->tail - hqueue->head);
+    }
+    else
+    {
+        return (unsigned int)(hqueue->buffer_length + hqueue->tail - hqueue->head);
+    }
+}
+
+//==============================================================================
+//  函数名称：Queue_Push
+//  函数功能：压入数据到队列中
+//  函数参数：
+//  函数返回：
+//==============================================================================
+QUEUE_StatusTypeDef Queue_Push(QUEUE_HandleTypeDef * hqueue, unsigned char data)
+{
+    unsigned int tmp = (hqueue->tail + 1) % hqueue->buffer_length;
+
+    if(tmp == hqueue->head)
+    {
+        return QUEUE_OVERLOAD;
+    }
+    else
+    {
+        hqueue->buffer[hqueue->tail] = data;
+        hqueue->tail = tmp;
+        return QUEUE_OK;
+    }
+}
+
+//==============================================================================
+//  函数名称：Queue_Push_Array
+//  函数功能：压入一组数据到队列中
+//  函数参数：
+//  函数返回：成功压入队列数据的数量
+//==============================================================================
+unsigned int Queue_Push_Array(QUEUE_HandleTypeDef * hqueue, unsigned char * pdatas, unsigned int len)
+{
+    unsigned int i;
+    for(i=0; i<len; i++)
+    {
+        if(Queue_Push(hqueue,pdatas[i]) == QUEUE_OVERLOAD)
+        {
+            break;
+        }
+    }
+    return i;
+}
+
---
+++ queue.h
@@ -0,0 +1,40 @@
+#ifndef __QUEUE_H
+#define __QUEUE_H
+
+//==============================================================================
+//  常量定义
+//==============================================================================
+
+
+//==============================================================================
+//  类型定义
+//==============================================================================
+typedef struct QUEUE_HandleTypeDef{
+    unsigned int head;                      //队列头指针
+    unsigned int tail;                      //队列尾指针
+    unsigned int buffer_length;             //队列缓存长度（初始化时赋值）
+    unsigned char * buffer;		              //队列缓存数组（初始化时赋值）
+}QUEUE_HandleTypeDef;
+
+typedef enum
+{
+    QUEUE_OK       = 0x00U,                 //队列OK
+    QUEUE_ERROR    = 0x01U,                 //队列错误
+    QUEUE_BUSY     = 0x02U,                 //队列忙
+    QUEUE_TIMEOUT  = 0x03U,                 //队列超时
+    QUEUE_OVERLOAD = 0x04U,                 //队列已满
+    QUEUE_VOID     = 0x05U                  //队列已空
+} QUEUE_StatusTypeDef;
+
+
+//==============================================================================
+//  公共函数声明
+//==============================================================================
+extern void Queue_Init(QUEUE_HandleTypeDef * hqueue, unsigned char * buffer, unsigned int len);
+extern void Queue_Clear(QUEUE_HandleTypeDef * hqueue);
+extern unsigned int Queue_Count(QUEUE_HandleTypeDef * hqueue);
+extern QUEUE_StatusTypeDef Queue_Push(QUEUE_HandleTypeDef * hqueue, unsigned char data);
+extern unsigned int Queue_Push_Array(QUEUE_HandleTypeDef * hqueue, unsigned char * pdatas, unsigned int len);
+
+#endif
+
